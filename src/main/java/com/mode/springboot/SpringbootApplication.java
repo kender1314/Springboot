07835242768bf0bce66dc6ecf51f0a4541023f70
kender1314 <37035331+kender1314@users.noreply.github.com>
2020-08-13 21:48:24 +0800

    更新

--- src/main/java/com/mode/springboot/SpringbootApplication.java
+++ src/main/java/com/mode/springboot/SpringbootApplication.java
@@ -3,6 +3,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author jiang.he
+ * @Version 1.0.0 RELEASE
+ * @Date 2020/7/11 20:12
+ * @Description:
+ */
 @SpringBootApplication
 @MapperScan("com.mode.springboot.dao")
 public class SpringbootApplication {
