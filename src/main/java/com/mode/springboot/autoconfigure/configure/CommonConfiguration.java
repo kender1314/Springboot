93efc4ef404ddd1a8608d4b95c699f980fc0ba42
hejiang <18715770814@163.com>
2020-08-19 09:59:47 +0800

    修改目录结构

--- src/main/java/com/mode/springboot/autoconfigure/configure/CommonConfiguration.java
+++ src/main/java/com/mode/springboot/autoconfigure/configure/CommonConfiguration.java
@@ -1,6 +1,5 @@
 package com.mode.springboot.autoconfigure.configure;
 
-import com.mode.springboot.autoconfigure.configuration.UserInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/mode/springboot/autoconfigure/configure/LoginInterceptors.java
+++ src/main/java/com/mode/springboot/autoconfigure/configure/LoginInterceptors.java
@@ -1,6 +1,5 @@
-package com.mode.springboot.autoconfigure.configuration;
+package com.mode.springboot.autoconfigure.configure;
 
-import com.mode.springboot.autoconfigure.configure.CommonProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
